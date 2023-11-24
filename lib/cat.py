b81b8660e38363a5f99a5f0eeb47b5028d7c288a
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-11-24 11:53:13 +0000

    Add Cat test for played_with, pass

--- lib/cat.py
+++ lib/cat.py
@@ -1,3 +1,4 @@
 class Cat():
     def __init__(self, name):
         self.name = name
+        self.played_with = False
--- tests/test_cat.py
+++ tests/test_cat.py
@@ -9,3 +9,13 @@ def test_cat_takes_name_as_argument_and_instantiates_with_name_property():
     expected = 'Garfield'
     # assertion
     assert actual == expected
+
+def test_cat_instantiates_with_played_with_to_false_property():
+    # setup / scenario
+    cat = Cat('Garfield')
+
+    # action
+    actual = cat.played_with
+    expected = False
+    # assertion
+    assert actual == expected
